75a6e901c502f4e5e10ced5064d6542efb7de985
cs3343grp7 <32590781+cs3343grp7@users.noreply.github.com>
2017-11-04 14:23:08 +0800

    test case of day
    test case of day

--- src/testcase/testA.java
+++ src/testcase/testA.java
@@ -37,18 +37,110 @@ public class testA {
 //
 //	}
 	@Test
-	public void test01() {
-		String input = "arrive B1 Core_Java";
-		String[] cmdParts = input.split(" ");
-		SystemDate.createTheInstance("03-Jan-2014");
-		Command command = new CmdArrive();
-		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-		System.setOut(new PrintStream(outContent));
-		try {
-			command.execute(cmdParts);
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-		} 
-		assertEquals("Done."+System.getProperty("line.separator"),outContent.toString());
+	public void testDay01() {
+		Day day= new Day(2017,1,1);
+		assertEquals(day.toString(),"1-Jan-2017");		
 	}
+	@Test
+	public void testDay02() {
+		Day day= new Day("1-Jan-2017");
+		assertEquals(day.toString(),"1-Jan-2017");		
+	}
+	@Test
+	public void testDayisLeapYear01() {
+		assertEquals(Day.isLeapYear(400),true);		
+	}
+	@Test
+	public void testDayisLeapYear02() {
+		assertEquals(Day.isLeapYear(100),false);		
+	}
+	@Test
+	public void testDayisLeapYear03() {
+		assertEquals(Day.isLeapYear(2016),true);		
+	}
+	@Test
+	public void testDayisLeapYear04() {
+		assertEquals(Day.isLeapYear(2017),false);		
+	}
+	@Test
+	public void testDayValid01() {
+		assertEquals(Day.valid(2017,1,1),true);		
+	}
+	@Test
+	public void testDayValid02() {
+		assertEquals(Day.valid(2017,1,32),false);		
+	}
+	@Test
+	public void testDayValid03() {
+		assertEquals(Day.valid(2017,1,0),false);		
+	}
+	@Test
+	public void testDayValid04() {
+		assertEquals(Day.valid(2016,2,29),true);		
+	}
+	@Test
+	public void testDayValid05() {
+		assertEquals(Day.valid(2017,2,29),false);		
+	}
+	@Test
+	public void testDayValid06() {
+		assertEquals(Day.valid(2017,4,31),false);		
+	}
+	@Test
+	public void testDayDatePassed01() {
+		Day day1= new Day(2017,1,1);
+		Day day2= new Day(2018,1,1);
+		assertEquals(day1.datePassed(day2),true);		
+	}
+	@Test
+	public void testDayDatePassed02() {
+		Day day1= new Day(2018,1,1);
+		Day day2= new Day(2017,1,1);
+		assertEquals(day1.datePassed(day2),false);		
+	}
+	@Test
+	public void testDayDatePassed03() {
+		Day day1= new Day(2017,1,1);
+		Day day2= new Day(2017,2,1);
+		assertEquals(day1.datePassed(day2),true);		
+	}
+	@Test
+	public void testDayDatePassed04() {
+		Day day1= new Day(2017,2,1);
+		Day day2= new Day(2017,1,1);
+		assertEquals(day1.datePassed(day2),false);		
+	}
+	@Test
+	public void testDayDatePassed05() {
+		Day day1= new Day(2017,1,1);
+		Day day2= new Day(2017,1,2);
+		assertEquals(day1.datePassed(day2),true);		
+	}
+	@Test
+	public void testDayDatePassed06() {
+		Day day1= new Day(2017,1,3);
+		Day day2= new Day(2017,1,1);
+		assertEquals(day1.datePassed(day2),false);		
+	}
+	@Test
+	public void testDayDatePassed07() {
+		Day day1= new Day(2017,1,1);
+		Day day2= new Day(2017,1,1);
+		assertEquals(day1.datePassed(day2),false);		
+	}
+//	@Test
+//	public void test01() {
+//		String input = "arrive B1 Core_Java";
+//		String[] cmdParts = input.split(" ");
+//		SystemDate.createTheInstance("03-Jan-2014");
+//		Command command = new CmdArrive();
+//		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+//		System.setOut(new PrintStream(outContent));
+//		try {
+//			command.execute(cmdParts);
+//		} catch (Exception e) {
+//			System.out.println(e.getMessage());
+//		} 
+//		assertEquals("Done."+System.getProperty("line.separator"),outContent.toString());
+//	}
 }
