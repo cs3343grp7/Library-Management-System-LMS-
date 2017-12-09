3d004b5ab3147a514b0d64226b5ab99b6300b3b4
cs3343grp7 <32590781+cs3343grp7@users.noreply.github.com>
2017-12-10 04:30:24 +0800

    FIFO done

--- src/library/Main.java
+++ src/library/Main.java
@@ -1,164 +1,179 @@
 package library;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 import java.util.*;
 
 
 public class Main {
 
-	public static void main(String [] args) throws FileNotFoundException, UnsupportedEncodingException
+	public static void main(String [] args) throws UnsupportedEncodingException
 	{	
 		
 		Scanner in = new Scanner(System.in);
-		
-		Scanner memberDataFile = new Scanner(new File("memberData.txt"));
-		Scanner bookDataFile = new Scanner(new File("bookData.txt"));
-		
-		ArrayList<String> memberFileLineList = new ArrayList<String>();
-		
-		while (memberDataFile.hasNext())
+		try
 		{
-			String memberFileInLine1 = memberDataFile.nextLine();
-			memberFileLineList.add(memberFileInLine1);
-			String[] memberFileInParts = memberFileInLine1.split("*");
+				Scanner memberDataFile = new Scanner(new File("memberData.txt"));
+				Scanner bookDataFile = new Scanner(new File("bookData.txt"));
+	
+			ArrayList<String> memberFileLineList = new ArrayList<String>();
 			
-			String mId = memberFileInParts[0];
-			String mName = memberFileInParts[1];
-			String joinDate = memberFileInParts[2];
-			int bCounts = Integer.parseInt(memberFileInParts[3]);
-			int rCounts = Integer.parseInt(memberFileInParts[4]);
-			
-			Member m = new Member(mId,mName, new MemberStatusNormal());
-			Library.getInstance().addMember(m);
-			
-			try 
+			while (memberDataFile.hasNext())
 			{
-				m.setJoinDate(new Day(joinDate));	
-			}
-			catch (ExDayNotValid e)
-			{
-				System.out.println(e.getMessage());
-			}
-			
-			m.setBorrowCounts(bCounts);
-			m.setRequestCounts(rCounts);
-			
-		}
-		
-		while (bookDataFile.hasNext())
-		{
-			String bookFileInLine1 = bookDataFile.nextLine();
-			String[] bookFileInParts = bookFileInLine1.split("*");
-			
-			String bId = bookFileInParts[0];
-			String bName = bookFileInParts[1];
-			String arrivalDate = bookFileInParts[2];
-			int queueListSize = Integer.parseInt(bookFileInParts[3]);
-			String queueListString = bookFileInParts[4];
-			int bookStatusNum = Integer.parseInt(bookFileInParts[5]);
-			
-			Book b;
-			
-			try
-			{
-				if (bookStatusNum == 0)
-					b = new Book(bId,bName, new BookStatusAvailable());
-				else if (bookStatusNum == 1)
+				String memberFileInLine1 = memberDataFile.nextLine();
+				memberFileLineList.add(memberFileInLine1);
+				String[] memberFileInParts = memberFileInLine1.split("*");
+				
+				String mId = memberFileInParts[0];
+				String mName = memberFileInParts[1];
+				String joinDate = memberFileInParts[2];
+				int bCounts = Integer.parseInt(memberFileInParts[3]);
+				int rCounts = Integer.parseInt(memberFileInParts[4]);
+				
+				Member m = new Member(mId,mName, new MemberStatusNormal());
+				Library.getInstance().addMember(m);
+				
+				try 
 				{
-					b = new Book(bId,bName, new BookStatusBorrowed());
-					
-					Member borrowMember = Library.getInstance().findMember(bookFileInParts[6]);
-					((BookStatusBorrowed)b.getBookStatus()).set(borrowMember, b);
-					
-					Day loanDate = new Day(bookFileInParts[7]);
-					Day loanDeadLineDate = loanDate.getLoanDeadLineDate();
-					
-					((BookStatusBorrowed)b.getBookStatus()).setLoanDate(loanDate);
-					((BookStatusBorrowed)b.getBookStatus()).setLoanDeadLineDate(loanDeadLineDate);
+					m.setJoinDate(new Day(joinDate));	
 				}
-				else
+				catch (ExDayNotValid e)
 				{
-					b = new Book(bId,bName, new BookStatusOnhold());
-					
-					Member onholdMember = Library.getInstance().findMember(bookFileInParts[6]);
-					((BookStatusOnhold)b.getBookStatus()).set(onholdMember, b);
-					
-					Day onholdDate = new Day(bookFileInParts[7]);
-					Day onholdDeadLineDate = onholdDate.getOnholdDeadLineDate();
-					
-					((BookStatusOnhold)b.getBookStatus()).setOnholdDate(onholdDate);
-					((BookStatusOnhold)b.getBookStatus()).setOnholdDeadLineDate(onholdDeadLineDate);
+					System.out.println(e.getMessage());
 				}
 				
-				Library.getInstance().addBook(b);
+				m.setBorrowCounts(bCounts);
+				m.setRequestCounts(rCounts);
 				
-				if (queueListSize > 0)
+			}
+			
+			while (bookDataFile.hasNext())
+			{
+				String bookFileInLine1 = bookDataFile.nextLine();
+				String[] bookFileInParts = bookFileInLine1.split("*");
+				
+				String bId = bookFileInParts[0];
+				String bName = bookFileInParts[1];
+				String arrivalDate = bookFileInParts[2];
+				int queueListSize = Integer.parseInt(bookFileInParts[3]);
+				String queueListString = bookFileInParts[4];
+				int bookStatusNum = Integer.parseInt(bookFileInParts[5]);
+				
+				Book b;
+				
+				try
 				{
-					String[] queueListParts = queueListString.split(" ");
-					for (int i=0; i<queueListSize; i++)
+					if (bookStatusNum == 0)
+						b = new Book(bId,bName, new BookStatusAvailable());
+					else if (bookStatusNum == 1)
 					{
-						b.addInQueueList(Library.getInstance().findMember(queueListParts[i]));
-					}
-				
-				}
+						b = new Book(bId,bName, new BookStatusBorrowed());
 						
-				b.setArrivalDate(new Day(arrivalDate));
-			}
-
-			catch (ExDayNotValid e)
-			{
-				System.out.println(e.getMessage());
-			} 
-			catch (ExMemberNotFound e) 
-			{
-				System.out.println(e.getMessage());
-			}
-			
-			System.out.println("Book Data Imported.");
-			
-		}
-		
-		
-		while (!memberFileLineList.isEmpty())
-		{
-			
-			String memberFileInLine1 = memberFileLineList.get(0);
-			memberFileLineList.remove(0);
-			String[] memberFileInParts = memberFileInLine1.split("*");
-			
-			Member m;
-			try 
-			{
-					m = Library.getInstance().findMember(memberFileInParts[0]);
-				
-				int memberStatusNum = Integer.parseInt(memberFileInParts[5]);
-				int suspendListSize = Integer.parseInt(memberFileInParts[6]);
-				String suspendListString = memberFileInParts[7];
-				
-				if (memberStatusNum == 1)
-				{
-					String[] suspendListParts = suspendListString.split(" ");
-					for (int i=0; i<suspendListSize; i++)
+						Member borrowMember = Library.getInstance().findMember(bookFileInParts[6]);
+						((BookStatusBorrowed)b.getBookStatus()).set(borrowMember, b);
+						
+						Day loanDate = new Day(bookFileInParts[7]);
+						Day loanDeadLineDate = loanDate.getLoanDeadLineDate();
+						
+						((BookStatusBorrowed)b.getBookStatus()).setLoanDate(loanDate);
+						((BookStatusBorrowed)b.getBookStatus()).setLoanDeadLineDate(loanDeadLineDate);
+					}
+					else
 					{
-						((MemberStatusSuspend)m.getMemberStatus()).addSuspendBook(Library.getInstance().findBook(suspendListParts[i]));
+						b = new Book(bId,bName, new BookStatusOnhold());
+						
+						Member onholdMember = Library.getInstance().findMember(bookFileInParts[6]);
+						((BookStatusOnhold)b.getBookStatus()).set(onholdMember, b);
+						
+						Day onholdDate = new Day(bookFileInParts[7]);
+						Day onholdDeadLineDate = onholdDate.getOnholdDeadLineDate();
+						
+						((BookStatusOnhold)b.getBookStatus()).setOnholdDate(onholdDate);
+						((BookStatusOnhold)b.getBookStatus()).setOnholdDeadLineDate(onholdDeadLineDate);
+					}
+					
+					Library.getInstance().addBook(b);
+					
+					if (queueListSize > 0)
+					{
+						String[] queueListParts = queueListString.split(" ");
+						for (int i=0; i<queueListSize; i++)
+						{
+							b.addInQueueList(Library.getInstance().findMember(queueListParts[i]));
+						}
+					
+					}
+							
+					b.setArrivalDate(new Day(arrivalDate));
+				}
+	
+				catch (ExDayNotValid e)
+				{
+					System.out.println(e.getMessage());
+				} 
+				catch (ExMemberNotFound e) 
+				{
+					System.out.println(e.getMessage());
+				}
+				
+				System.out.println("Book Data Imported.");
+				
+			}
+			
+			
+			while (!memberFileLineList.isEmpty())
+			{
+				
+				String memberFileInLine1 = memberFileLineList.get(0);
+				memberFileLineList.remove(0);
+				String[] memberFileInParts = memberFileInLine1.split("*");
+				
+				Member m;
+				try 
+				{
+						m = Library.getInstance().findMember(memberFileInParts[0]);
+					
+					int memberStatusNum = Integer.parseInt(memberFileInParts[5]);
+					int suspendListSize = Integer.parseInt(memberFileInParts[6]);
+					String suspendListString = memberFileInParts[7];
+					
+					if (memberStatusNum == 1)
+					{
+						String[] suspendListParts = suspendListString.split(" ");
+						for (int i=0; i<suspendListSize; i++)
+						{
+							((MemberStatusSuspend)m.getMemberStatus()).addSuspendBook(Library.getInstance().findBook(suspendListParts[i]));
+						}
 					}
 				}
-			}
-			catch (ExMemberNotFound e) 
-			{
-				System.out.println(e.getMessage());
-			}
-			catch (ExBookNotFound e)
-			{
-				System.out.println(e.getMessage());
+				catch (ExMemberNotFound e) 
+				{
+					System.out.println(e.getMessage());
+				}
+				catch (ExBookNotFound e)
+				{
+					System.out.println(e.getMessage());
+				}
+				
+				System.out.println("Member Data Imported.");
 			}
 			
-			System.out.println("Member Data Imported.");
+			memberDataFile.close();
+			bookDataFile.close();
 		}
-		
-		memberDataFile.close();
-		bookDataFile.close();
+		catch (FileNotFoundException e)
+		{
+			System.out.println("Data file not found, System is going to create them.");
+			try {
+				PrintWriter memberDataWriter = new PrintWriter("memberData.txt", "UTF-8");
+				PrintWriter bookDataWriter = new PrintWriter("bookData.txt", "UTF-8");
+				memberDataWriter.close();
+				bookDataWriter.close();
+			} catch (FileNotFoundException e1) {
+				System.out.println("Cannot Create datat txt file!");
+			}
+		}
 		
 		Calendar calendar = Calendar.getInstance();
 		String today = calendar.getTime().toString();
@@ -181,8 +196,13 @@ public class Main {
 			System.out.print("\n> ");
 			cmdLine = in.nextLine();
 			if (cmdLine.equals("")) continue;  
+			
 			if (cmdLine.equals("quit")){
-				Library.getInstance().saveData();
+				try {
+					Library.getInstance().saveData();
+				} catch (FileNotFoundException e) {
+					System.out.println(e.getMessage());
+				}
 				System.out.print("END");
 				break; 
 			}
